da45bcba24a07a468a724d9dd2c43d1cf10e3194
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-01-23 16:37:13 +0100

    feat: Ajout d'un service de gestion des messages d'or

--- src/main/java/services/MessageService.java
+++ src/main/java/services/MessageService.java
@@ -1,14 +1,58 @@
 package services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import modeles.MessageDor;
 
 public class MessageService {
-	public List<MessageDor> getAllMessageDor() {
+	/**
+	 * Retourne la liste des messages d'or
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public List<MessageDor> getAllMessageDor() throws Exception {
 		List<MessageDor> messageList = new ArrayList<>();
 		
+		Connection connection = Database.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Messages");
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		while (resultSet.next()) {
+			MessageDor messageDor = recupererMessageDor(resultSet);
+			
+			if (Objects.nonNull(messageDor)) {
+				messageList.add(messageDor);
+			}
+		}
+		
 		return messageList;
+	}
+
+	/**
+	 * Récupère un objet MessageDor à partir d'un resultSet
+	 * 
+	 * @param resultSet
+	 * @return
+	 * @throws SQLException
+	 */
+	private MessageDor recupererMessageDor(ResultSet resultSet) throws SQLException {
+		MessageDor messageDor = null;
+		
+		if (Objects.nonNull(resultSet)) {
+			messageDor = new MessageDor();
+			
+			messageDor.setNumMsg(resultSet.getInt("NUMMSG"));
+			messageDor.setPseudo(resultSet.getString("PSEUDO"));
+			messageDor.setMessage(resultSet.getString("MESSAGE"));
+		}
+		
+		return messageDor;
 	}
 }
