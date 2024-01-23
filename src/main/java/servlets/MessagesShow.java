d061a4839c7c6a7a94476f050ce4e8028976ddd5
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-01-23 16:39:18 +0100

    feat: Ajout des messages dans le servlet

--- src/main/java/servlets/MessagesShow.java
+++ src/main/java/servlets/MessagesShow.java
@@ -1,17 +1,33 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import modeles.MessageDor;
+import services.MessageService;
+
 @WebServlet("/messages")
 public class MessagesShow extends CommonServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private MessageService messageService = new MessageService();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		List<MessageDor> listeMessages = new ArrayList<>();
+		
+		try {
+			listeMessages = messageService.getAllMessageDor();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("messages", listeMessages);
 		view("messages", request, response);
 	}
 
