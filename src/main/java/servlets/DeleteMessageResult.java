ab95ab19dbe6fead0b2fd128bf5aebe4e5138cd7
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-01-29 14:19:03 +0100

    ref: Optimisation des imports

--- src/main/java/servlets/DeleteMessageResult.java
+++ src/main/java/servlets/DeleteMessageResult.java
@@ -1,21 +1,16 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import modeles.MessageDor;
 import services.MessageService;
 
 @WebServlet("/delete/result")
